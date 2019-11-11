312e809fe67e48f7bbe217476a5bc03e587135e7
Richard Fu <richardf@synnex.com>
2019-11-11 16:38:08 +0800

    update

---
+++ basic/src/main/java/com/richard/demo/basic/ExcludeInitializer.java
@@ -0,0 +1,38 @@
+package com.richard.demo.basic;
+
+import com.richard.demo.basic.util.BasicUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.LinkedHashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ExcludeInitializer implements ApplicationContextInitializer {
+
+    private final static Set<String> EXCLUDE_SET = BasicUtil.setOf(
+            "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+            "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
+            "org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration");
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        MapPropertySource source = getExcludeSource(environment);
+        source.getSource().put("spring.autoconfigure.exclude", String.join(",", EXCLUDE_SET));
+    }
+
+    private MapPropertySource getExcludeSource(ConfigurableEnvironment configurableEnvironment) {
+        MutablePropertySources sources = configurableEnvironment.getPropertySources();
+        MapPropertySource source = (MapPropertySource) sources.get("defaultConfig");
+        if (source == null) {
+            source = new MapPropertySource("defaultConfig", new LinkedHashMap<>());
+            sources.addLast(source);
+        }
+        return source;
+    }
+}
